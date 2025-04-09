612ce718701e1769efe4f261d696d6773f08d5b7
cbgnoceara <softwareigreja12@gmail.com>
2025-04-09 16:36:08 -0300

    meu Deus

--- home/telainicial.js
+++ home/telainicial.js
@@ -71,10 +71,7 @@ async function fetchReservas() {
       const item = document.createElement("li");
       item.classList.add("reserva-item");
 
-      const dataHoraInicio = new Date(`${reserva.dataInicio}T${reserva.horarioInicio}`);
-      const dataHoraFim = new Date(`${reserva.dataFim}T${reserva.horarioFim}`);
-
-      const dataHoraInicioFormatada = dataHoraInicio.toLocaleString("pt-BR", {
+      const dataHoraInicioFormatada = new Date(reserva.dataHoraInicio).toLocaleString("pt-BR", {
         timeZone: "America/Fortaleza",
         day: "2-digit",
         month: "2-digit",
@@ -83,7 +80,7 @@ async function fetchReservas() {
         minute: "2-digit"
       });
 
-      const dataHoraFimFormatada = dataHoraFim.toLocaleString("pt-BR", {
+      const dataHoraFimFormatada = new Date(reserva.dataHoraFim).toLocaleString("pt-BR", {
         timeZone: "America/Fortaleza",
         day: "2-digit",
         month: "2-digit",
